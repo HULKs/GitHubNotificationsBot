d66a70b1f1b4e2876f3085930c2832f4e8b21488
Hendrik <git@h3ndrk.de>
2020-12-02 12:11:23 +0100

    Add options

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,12 @@
 import click
 
-@click.command()
-def run(**arguments):
-    print(arguments)
 
-def main():
-    run(auto_envvar_prefix='BOT')
+@click.command()
+@click.option('--telegram-bot-token', required=True, envvar='TELEGRAM_BOT_TOKEN')
+@click.option('--telegram-chat-id', required=True, envvar='TELEGRAM_CHAT_ID')
+@click.option('--matrix-user-id', required=True, envvar='MATRIX_USER_ID')
+@click.option('--matrix-homeserver', required=True, envvar='MATRIX_HOMESERVER')
+@click.option('--matrix-device-id', required=True, envvar='MATRIX_DEVICE_ID')
+@click.option('--matrix-access-token', required=True, envvar='MATRIX_ACCESS_TOKEN')
+def main(**arguments):
+    print(arguments)

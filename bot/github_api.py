90127c4d306455f066c98a3c0c812d9288c141e9
Hendrik <git@h3ndrk.de>
2020-12-22 17:06:53 +0100

    Add hook creation/removal to GitHubApi

--- bot/github_api.py
+++ bot/github_api.py
@@ -17,16 +17,6 @@ class UnexpectedResponseStatus(Exception):
 
 class GitHubApi:
 
-    events = [
-        'push',
-        'issues',
-        'pull_request',
-        'issue_comment',
-        'pull_request_review_comment',
-        'pull_request_review',
-        'fork',
-    ]
-
     def __init__(self, access_token: str, *args, **kwargs):
         self.logger = logging.getLogger('GitHubApi')
         self.session = aiohttp.ClientSession(*args, **kwargs)
@@ -42,31 +32,31 @@ class GitHubApi:
     async def forks(self, owner: str, repo: str):
         '''Retrieves forks recursively'''
         self.logger.info(f'Retrieving forks recursively for {owner}/{repo}...')
-        url = f'https://api.github.com/repos/{owner}/{repo}/forks'
+        request_url = f'https://api.github.com/repos/{owner}/{repo}/forks'
         result = []
-        while url is not None:
-            self.logger.debug(f'GET {url}...')
-            async with self.session.get(url, headers={
+        while request_url is not None:
+            self.logger.debug(f'GET {request_url}...')
+            async with self.session.get(request_url, headers={
                 'Accept': 'application/vnd.github.v3+json',
                 'Authorization': f'token {self.access_token}',
                 'User-Agent': 'bot',
             }) as response:
-                self.logger.debug(f'GET {url} -> {response.status}')
+                self.logger.debug(f'GET {request_url} -> {response.status}')
                 if response.status != 200:
-                    raise UnexpectedResponseStatus('GET', url, response.status, 200, await response.text())
+                    raise UnexpectedResponseStatus('GET', request_url, response.status, 200, await response.text())
                 for fork in await response.json():
                     self.logger.info(
                         f'Found fork {fork["owner"]["login"]}/{fork["name"]}',
                     )
                     result.append((fork['owner']['login'], fork['name']))
-                url = None
+                request_url = None
                 if 'Link' in response.headers:
                     extracted_links = links_from_header.extract(
                         response.headers['Link'],
                     )
                     if 'next' in extracted_links:
                         self.logger.debug('Retrieving next page...')
-                        url = extracted_links['next']
+                        request_url = extracted_links['next']
         for fork_owner, fork_repo in result:
             self.logger.debug(f'Recurse into {fork_owner}/{fork_repo}...')
             result += await self.forks(fork_owner, fork_repo)
@@ -76,29 +66,67 @@ class GitHubApi:
         self.logger.info(
             f'Retrieving hooks for {owner_or_org}/{repo}...' if repo is not None else f'Retrieving hooks for {owner_or_org}...',
         )
-        url = f'https://api.github.com/repos/{owner_or_org}/{repo}/hooks' if repo is not None else f'https://api.github.com/orgs/{owner_or_org}/hooks'
+        request_url = f'https://api.github.com/repos/{owner_or_org}/{repo}/hooks' if repo is not None else f'https://api.github.com/orgs/{owner_or_org}/hooks'
         result = []
-        while url is not None:
-            self.logger.debug(f'GET {url}...')
-            async with self.session.get(url, headers={
+        while request_url is not None:
+            self.logger.debug(f'GET {request_url}...')
+            async with self.session.get(request_url, headers={
                 'Accept': 'application/vnd.github.v3+json',
                 'Authorization': f'token {self.access_token}',
                 'User-Agent': 'bot',
             }) as response:
-                self.logger.debug(f'GET {url} -> {response.status}')
+                self.logger.debug(f'GET {request_url} -> {response.status}')
                 if response.status != 200:
-                    raise UnexpectedResponseStatus('GET', url, response.status, 200, await response.text())
+                    raise UnexpectedResponseStatus('GET', request_url, response.status, 200, await response.text())
                 for hook in await response.json():
                     self.logger.info(
-                        f'Found hook {hook["config"]["url"]} (events: {hook["events"]})',
+                        f'Found hook {hook["id"]}: {hook["config"]["url"]} (events: {hook["events"]})',
                     )
-                    result.append((hook['config']['url'], hook['events']))
-                url = None
+                    result.append((hook['id'], hook['config']['url'], hook['events']))
+                request_url = None
                 if 'Link' in response.headers:
                     extracted_links = links_from_header.extract(
                         response.headers['Link'],
                     )
                     if 'next' in extracted_links:
                         self.logger.debug('Retrieving next page...')
-                        url = extracted_links['next']
+                        request_url = extracted_links['next']
         return result
+
+    async def create_hook(self, url: str, events: typing.List[str], secret: str, owner_or_org: str, repo: typing.Optional[str] = None):
+        self.logger.info(
+            f'Creating hook for {owner_or_org}/{repo} ({url}, {events})...' if repo is not None else f'Creating hook for {owner_or_org} ({url}, {events})...',
+        )
+        request_url = f'https://api.github.com/repos/{owner_or_org}/{repo}/hooks' if repo is not None else f'https://api.github.com/orgs/{owner_or_org}/hooks'
+        self.logger.debug(f'POST {request_url}...')
+        async with self.session.post(request_url, headers={
+            'Accept': 'application/vnd.github.v3+json',
+            'Authorization': f'token {self.access_token}',
+            'User-Agent': 'bot',
+        }, json={
+            'name': 'web',
+            'config': {
+                'url': url,
+                'content_type': 'json',
+                'secret': secret,
+            },
+            'events': events,
+        }) as response:
+            self.logger.debug(f'POST {url} -> {response.status}')
+            if response.status != 201:
+                raise UnexpectedResponseStatus('POST', url, response.status, 201, await response.text())
+
+    async def delete_hook(self, hook_id: int, owner_or_org: str, repo: typing.Optional[str] = None):
+        self.logger.info(
+            f'Delete hook {hook_id} from {owner_or_org}/{repo}...' if repo is not None else f'Delete hook {hook_id} from {owner_or_org}...',
+        )
+        request_url = f'https://api.github.com/repos/{owner_or_org}/{repo}/hooks/{hook_id}' if repo is not None else f'https://api.github.com/orgs/{owner_or_org}/hooks/{hook_id}'
+        self.logger.debug(f'DELETE {request_url}...')
+        async with self.session.delete(request_url, headers={
+            'Accept': 'application/vnd.github.v3+json',
+            'Authorization': f'token {self.access_token}',
+            'User-Agent': 'bot',
+        }) as response:
+            self.logger.debug(f'DELETE {request_url} -> {response.status}')
+            if response.status != 204:
+                raise UnexpectedResponseStatus('DELETE', request_url, response.status, 204, await response.text())
